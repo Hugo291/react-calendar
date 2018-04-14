9a67f9514747d964dcb8637b3cc3a4b446d5c06b
root <hugoferreirapro803@gmail.com>
2018-04-14 16:31:54 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Product extends React.Component{
+
+    state = {
+        quantity:0
+    };
+
+    addToCart(){
+        this.setState({
+            quantity : this.state.quantity+1
+        });
+        console.log('add'+this.state.quantity);
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>Quantity : {this.state.quantity}</p>
+                <button onClick={()=> {
+                    this.addToCart();
+                }} >Add to cart</button>
+            </div>
+        )
+    }
+
+}
+
+function App(){
+    return (
+        <div>
+            <Product name="First"/>
+            <Product name="Second"/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
